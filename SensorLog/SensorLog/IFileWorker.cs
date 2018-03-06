0eafd450c170cb5f7c7ac7c80ce668a0923add93
mr_dev <devmsv@gmail.com>
2018-03-06 02:20:30 +0200

    Add IFileWorker

---
+++ SensorLog/SensorLog/IFileWorker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface IFileWorker {
+    Task<bool> ExistsAsync(string filename); // проверка существования файла
+    Task SaveTextAsync(string filename, string text);   // сохранение текста в файл
+    Task<string> LoadTextAsync(string filename);  // загрузка текста из файла
+    Task<IEnumerable<string>> GetFilesAsync();  // получение файлов из определнного каталога
+    Task DeleteAsync(string filename);  // удаление файла
+    string GetFilePath(string filename);
+}
